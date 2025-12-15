073ed6d7d3ab019568048ce9dadacc28ddc923ac
janettgarciiaa <jg13465n@pace.edu>
2025-12-14 23:07:27 -0500

    Add files via upload

---
+++ db_connect.py
@@ -0,0 +1,16 @@
+import psycopg2
+
+def connect_db():
+    try:
+        conn = psycopg2.connect(
+            dbname="ProjectDB",
+            user="postgres",
+            password="Jarcia1221",
+            host="localhost",
+            port="5433"
+        )
+        print("Connected to ProjectDB successfully.")
+        return conn
+    except Exception as e:
+        print("Database connection failed:", e)
+        return None
